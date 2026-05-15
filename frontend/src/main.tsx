4a6bc3b3f6a3dd6afbc241d3a9e0adbaa3b35420
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-05-16 01:02:33 +0530

    chore: preload routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,11 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  defaultPreloadDelay: 50,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
